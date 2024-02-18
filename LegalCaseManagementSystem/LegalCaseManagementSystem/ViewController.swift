c3f8b4440ff70f6aeebf67f460d87136a8b40e05
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-02-18 13:42:42 -0600

    Basic UI design phase1

---
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LegalCaseManagementSystem
+//
+//  Created by ajit hk on 18/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
