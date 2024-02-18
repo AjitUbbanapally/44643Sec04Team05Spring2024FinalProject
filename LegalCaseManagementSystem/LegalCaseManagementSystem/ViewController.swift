74e0641124ed0e172887add6130738a98d020271
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-02-18 14:30:25 -0600

    Basic UI Design phase1

--- LegalCaseManagementSystem/LegalCaseManagementSystem/ViewController.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/ViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var UserNameTF: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTF: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    
 
 
 }
