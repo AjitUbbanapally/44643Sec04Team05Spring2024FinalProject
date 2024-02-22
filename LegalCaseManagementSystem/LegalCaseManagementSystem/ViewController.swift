fc679e65f38a2094b4d5a12de517a595a6fb34b6
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-02-21 20:37:49 -0600

    Constraints, gradient views

--- LegalCaseManagementSystem/LegalCaseManagementSystem/ViewController.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AnimatedGradientView
 
 class ViewController: UIViewController {
     
@@ -19,6 +20,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let animatedGradient = AnimatedGradientView(frame: view.bounds)
+        animatedGradient.direction = .up
+        animatedGradient.animationValues = [(colors: ["# A3D256", "#A3D256"], .up, .axial),
+                                            (colors: ["#A3D256", "#A3D256", "#A3D256"], .right, .axial),
+                                            (colors: ["#A3D256", "#A3D256"], .down, .axial),
+                                            (colors: ["#A3D256", "#A3D256", "#A3D256"], .left, .axial)]
+        view.addSubview(animatedGradient)
+        view.sendSubviewToBack(animatedGradient)
+        
     }
     
     
