ddccbe8441ccbdbfa57f946df7f12e9e4a711a6c
nannehussain <nannehussainshaik333@gmail.com>
2024-02-22 17:53:34 -0600

    Added some elements

---
+++ LegalCaseManagementSystem/LCMSVC.swift
@@ -0,0 +1,29 @@
+//
+//  LCMSVC.swift
+//  LegalCaseManagementSystem
+//
+//  Created by Hussain on 2/22/24.
+//
+
+import UIKit
+
+class LCMSVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
