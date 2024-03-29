d59d2e83a1d9f324673b1fc42cc1f4812666fe93
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2024-03-29 14:39:37 -0500

    Updated changes to Dashboard VC and Main.storyboard

--- LegalCaseManagementSystem/LegalCaseManagementSystem/DashboardVC.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/DashboardVC.swift
@@ -15,7 +15,23 @@ class DashboardVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var partyImage: UIImageView!
+    @IBAction func partyButton(_ sender: Any) {
+    }
+    @IBAction func MatterButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var MatterImage: UIImageView!
+    @IBOutlet weak var ReportsImage: UIImageView!
+    
+    @IBAction func ReportsButton(_ sender: Any) {
+    }
+    @IBOutlet weak var ProfileScoreImage: UIImageView!
+    
+    @IBAction func ProfileScoreButton(_ sender: Any) {
+    }
+    
+    
     /*
     // MARK: - Navigation
 
@@ -27,3 +43,8 @@ class DashboardVC: UIViewController {
     */
 
 }
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
