7ade377f20e9268b2ea43ab61f0918f777aeae7b
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2024-03-29 14:22:57 -0500

    Added ML Model to the project for check profile score prediction

--- LegalCaseManagementSystem/LegalCaseManagementSystem/ProfileScoreVC.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/ProfileScoreVC.swift
@@ -30,7 +30,7 @@ class ProfileScoreVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if(partyName==null!){
+        if(partyName.text==null){
             gender.isEnabled=false
             dateofbirth.isEnabled=false
             ssn.isEnabled=false
@@ -38,6 +38,42 @@ class ProfileScoreVC: UIViewController {
             noOfConvictedFelonies.isEnabled=false
         }else{
             partyName.isEnabled=true
+        }
+        
+       
+        var model: YourCoreMLModelClass?
+
+            override func viewDidLoad() {
+                super.viewDidLoad()
+
+             
+                if let modelURL = Bundle.main.url(forResource: "YourCoreMLModel", withExtension: "mlmodelc") {
+                    do {
+                        model = try YourCoreMLModelClass(contentsOf: modelURL)
+                       
+                        makePrediction()
+                    } catch {
+                        print("Error loading CoreML model: \(error.localizedDescription)")
+                    }
+                } else {
+                    print("CoreML model file not found.")
+                }
+            }
+
+            func makePrediction() {
+              
+                if let model = model {
+                    do {
+                        let inputData = YourInputDataStruct()
+                        let prediction = try model.prediction(input: inputData)
+                        print("Prediction result: \(prediction)")
+                    } catch {
+                        print("Error making prediction: \(error.localizedDescription)")
+                    }
+                } else {
+                    print("CoreML model is nil.")
+                }
+            }
         }
         
         
