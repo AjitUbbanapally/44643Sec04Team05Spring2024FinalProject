809b25a328a67ef741625ac7643fd93c150eb49b
SunnithReddyKondam <142992377+sunnithreddykondam@users.noreply.github.com>
2024-03-29 14:26:53 -0500

    Made changes to the Party VC

--- LegalCaseManagementSystem/LegalCaseManagementSystem/ProfileScoreVC.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/ProfileScoreVC.swift
@@ -41,45 +41,45 @@ class ProfileScoreVC: UIViewController {
         }
         
        
-        var model: YourCoreMLModelClass?
-
-            override func viewDidLoad() {
-                super.viewDidLoad()
-
-             
-                if let modelURL = Bundle.main.url(forResource: "YourCoreMLModel", withExtension: "mlmodelc") {
-                    do {
-                        model = try YourCoreMLModelClass(contentsOf: modelURL)
-                       
-                        makePrediction()
-                    } catch {
-                        print("Error loading CoreML model: \(error.localizedDescription)")
-                    }
-                } else {
-                    print("CoreML model file not found.")
-                }
-            }
-
-            func makePrediction() {
-              
-                if let model = model {
-                    do {
-                        let inputData = YourInputDataStruct()
-                        let prediction = try model.prediction(input: inputData)
-                        print("Prediction result: \(prediction)")
-                    } catch {
-                        print("Error making prediction: \(error.localizedDescription)")
-                    }
-                } else {
-                    print("CoreML model is nil.")
-                }
-            }
-        }
-        
-        
-        
-        
-     
+//        var model: YourCoreMLModelClass?
+//
+//            override func viewDidLoad() {
+//                super.viewDidLoad()
+//
+//             
+//                if let modelURL = Bundle.main.url(forResource: "YourCoreMLModel", withExtension: "mlmodelc") {
+//                    do {
+//                        model = try YourCoreMLModelClass(contentsOf: modelURL)
+//                       
+//                        makePrediction()
+//                    } catch {
+//                        print("Error loading CoreML model: \(error.localizedDescription)")
+//                    }
+//                } else {
+//                    print("CoreML model file not found.")
+//                }
+//            }
+//
+//            func makePrediction() {
+//              
+//                if let model = model {
+//                    do {
+//                        let inputData = YourInputDataStruct()
+//                        let prediction = try model.prediction(input: inputData)
+//                        print("Prediction result: \(prediction)")
+//                    } catch {
+//                        print("Error making prediction: \(error.localizedDescription)")
+//                    }
+//                } else {
+//                    print("CoreML model is nil.")
+//                }
+//            }
+//        }
+//        
+//        
+//        
+//        
+//     
                 
             
 
