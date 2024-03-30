cde107d2cd66067e341484a007ef5b6502802ac8
AjitUbbanapally <ajitpurnahk@gmail.com>
2024-03-29 20:47:47 -0500

    SplitVIew for UI page

--- LegalCaseManagementSystem/LegalCaseManagementSystem/MatterVC.swift
+++ LegalCaseManagementSystem/LegalCaseManagementSystem/MatterVC.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Eureka
 
-class MatterVC: UIViewController {
+class MatterVC: FormViewController {
     
     
     @IBOutlet weak var ClientTableView: UITableView!
@@ -15,14 +16,104 @@ class MatterVC: UIViewController {
     var client  = ["Matter1","Matter2","Matter3","Matter4"]
     
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
 
         // Do any additional setup after loading the view.
         
         //ClientTableView.delegate = self
       //  ClientTableView.dataSource = self
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newForm()
+        
     }
+    
+    func newForm(){
+            
+            form +++ Section()
+                   
+                   <<< LabelRow() {
+                       $0.title = "The details of matters"
+                       $0.cellUpdate { cell, row in
+                                       cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+                                       cell.textLabel?.numberOfLines = 0
+                                       cell.textLabel?.textAlignment = .center
+                                   }
+                   }
+                   
+                   +++ Section("ADMISSION FACTORS")
+                   
+        <<< SliderRow("GRE") {
+                           $0.title = "GRE Score"
+                           $0.value = 0.0
+                           $0.cell.slider.minimumValue = 260.0
+                           $0.cell.slider.maximumValue = 340.0
+                           $0.cell.slider.value = 5
+                       }
+                       
+                       <<< SliderRow("TOEFL") {
+                           $0.title = "TOEFL Score"
+                           $0.value = 0.0
+                           $0.cell.slider.minimumValue = 0.0
+                           $0.cell.slider.maximumValue = 120.0
+                           $0.cell.slider.value = 5
+                       }
+                       
+                      
+                       
+                       <<< SegmentedRow<String>("UniversityRating") {
+                           $0.title = "University Rating"
+                           $0.options = ["1", "2", "3", "4", "5"]
+                           $0.value = "1"
+                       }
+                       
+                       <<< SegmentedRow<String>("SOP") {
+                           $0.title = "SOP"
+                           $0.options = ["1", "2", "3", "4", "5"]
+                           $0.value = "1"
+                       }
+                       
+                       <<< SegmentedRow<String>("LOR") {
+                           $0.title = "LOR"
+                           $0.options = ["1", "2", "3", "4", "5"]
+                           $0.value = "1"
+                       }
+                
+                      <<< SliderRow("CGPA") {
+                           $0.title = "CGPA"
+                           $0.value = 0.0
+                           $0.cell.slider.minimumValue = 0.0
+                           $0.cell.slider.maximumValue = 10.0
+                       }
+                       
+        
+                   
+                   <<< SwitchRow("ResearchInterests") {
+                       $0.title = "Research"
+                       $0.value = false
+                   }
+                   
+                   +++ Section("ACTIONS")
+                   
+                   <<< ButtonRow() {
+                       $0.title = "Predict"
+                       $0.onCellSelection { cell, row in
+                           self.predictprobabilityAdmissionAcceptance()
+                       }
+                   }
+                   
+                   <<< ButtonRow() {
+                       $0.title = "Clear"
+                       $0.onCellSelection { cell, row in
+                           self.resetForm()
+                       }
+                   }
+               }
+    
+    
     
     
     
